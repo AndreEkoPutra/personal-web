606e90c111c74029f286b3eb0ab1140e205d3a95
AndreEkoPutra <h1101221059@student.untan.ac.id>
2025-08-09 19:22:36 +0700

    perbaikan about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,46 +1,33 @@
 import profil from "../assets/logo/profil.png"
 function About() {
   return (
-    <div>
-      <section id="about" className="h-screen flex">
-        <div className="w-50 h-50 ">
-            <img className="rounded-2xl" src={profil}/>
-            <h1>My Photo Profil</h1>
-        </div>
-        <div>
-            <h1>About Me</h1>
-            <h1>I,m <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-indigo-100">Andreanus Eko Putra</span>, a Web Dev & Mobile Dev</h1>
-            <p>Aku adalah mahasiswa Untan dari program studi sistem informasi</p>
-            <p>Asal saya dari ponti, dan saya adalaah orang amang</p>
-            <div className="flex space-x-5">
-                <div className="flex py-1 rounded-lg border border-white font-bold">
-                    <div className="w-7 h-7"><img src="https://www.citypng.com/public/uploads/preview/linkedin-square-white-icon-transparent-png-701751694974206tsxnbf2rft.png"/></div>
-                    <h1>Dusun Amang</h1>
-                </div>
-                <div className="flex py-1 rounded-lg border border-white font-bold">
-                    <div className="w-7 h-7"><img src="https://www.citypng.com/public/uploads/preview/linkedin-square-white-icon-transparent-png-701751694974206tsxnbf2rft.png"/></div>
-                    <h1>h1101221059@student.untan.ac.id</h1>
-                </div>
+    <section id="about" className="h-screen flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-indigo-100">About Me</h1>
+        <div className="flex">
+            <div className="kiri w-60 h-60">
+                <img className="bg-gradient-to-t from-indigo-800 to-indigo-500 border-2 border-indigo-200 rounded-3xl" src={profil} />
             </div>
-            <div className="flex space-x-5">
-                <div className="flex py-1 rounded-lg border border-white font-bold">
-                    <div className="w-7 h-7"><img src="https://www.citypng.com/public/uploads/preview/linkedin-square-white-icon-transparent-png-701751694974206tsxnbf2rft.png"/></div>
-                    <h1>083152005982</h1>
-                </div>
-                <div className="flex py-1 rounded-lg border border-white font-bold">
-                    <div className="w-7 h-7"><img src="https://www.citypng.com/public/uploads/preview/linkedin-square-white-icon-transparent-png-701751694974206tsxnbf2rft.png"/></div>
-                    <h1>Sistem Informasi, Universitas Tanjungpura</h1>
-                </div>
-            </div>
-            <div className="">
-                <ul className="flex space-x-5">
-                    <li className="px-4 py-1 rounded-lg border border-white font-bold text-blue-300 hover:bg-gradient-to-r hover:from-indigo-400 hover:to-indigo-100 hover:text-black transition-colors duration-300 cursor-pointer"><a href="www.google.com" target="_blank">Download CV</a></li>
-                    <li className="px-4 py-1 rounded-lg border border-white font-bold text-blue-300 hover:bg-gradient-to-r hover:from-indigo-400 hover:to-indigo-100 hover:text-black transition-colors duration-300 cursor-pointer"><a href="www.facebook.com" target="_blank">Hire Me</a></li>
+
+
+            <div className="kanan pl-11">
+                <h1>I'm <span>Andreanus Eko Putra</span>, a Web Developer & Android Developer</h1>
+                <p>Sayay adalah seorang mahasiswa</p>
+                <p>Ketertarikan saya adalah pada web dan android</p>
+                <ul className="flex space-x-1 space-y-1">
+                    <li className="border-2 border-red-100 w-50 h-10 flex items-center">1</li>
+                    <li className="border-2 border-red-100 w-50 h-10 flex items-center">2</li>
+                </ul>
+                <ul className="flex space-x-1 space-y-1">
+                    <li className="border-2 border-red-100 w-50 h-10 flex items-center">3</li>
+                    <li className="border-2 border-red-100 w-50 h-10 flex items-center">4</li>
+                </ul>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#home">Home</a></li>
                 </ul>
             </div>
         </div>
-      </section>
-    </div>
+    </section>
   )
 }
 
